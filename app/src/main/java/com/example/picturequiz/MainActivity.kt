74fee796a2b5b4f106a4ecf11fbe64a7fe12c969
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-02-03 12:53:10 +0100

    Added the OnClickListener

--- app/src/main/java/com/example/picturequiz/MainActivity.kt
+++ app/src/main/java/com/example/picturequiz/MainActivity.kt
@@ -10,6 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnConfirm.setOnClickListener {
+            checkAnswer()
+        }
+
     }
 
     /**
