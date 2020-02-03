b1c8e9ad5052b56228b2dc268d19765983746ddf
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-02-03 12:52:09 +0100

    Added the checkAnswer function

--- app/src/main/java/com/example/picturequiz/MainActivity.kt
+++ app/src/main/java/com/example/picturequiz/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.picturequiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    /**
+     * Check if the submitted answer is correct.
+     */
+    private fun checkAnswer() {
+        val answer = etAnswer.text.toString()
+
+        // When the answer equals "giraffe" then display a correct message using a toast message.
+        // Otherwise display an incorrect message.
+        if (answer == getString(R.string.giraffe)) {
+            Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
